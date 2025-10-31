0cd04f79339267d911f5fe984ff55e495c19df4d
Alherma7 <ahernandezmai@uoc.edu>
2025-10-31 14:47:10 +0100

    Update save.py

--- PRACT-1/src/save.py
+++ PRACT-1/src/save.py
@@ -1,19 +1,18 @@
 #save.py
-import csv
 
 def save_recipes(recipes, csv_path="Recetas.csv"):
 
     with open(csv_path, "w", encoding="utf-8", newline="") as f_csv:
         writer = csv.writer(f_csv, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow([
-            "url", "pais", "titulo", "fecha", "autor", "descripcion_corta", "calificacion_media", "num_reviews",
-            "preparacion", "ingredientes", "categoria_principal", "nutrition"
+            "url", "pais", "titulo", "fecha", "autor", "calificacion_media", "num_reviews",
+            "realizado_casa","preparacion", "ingredientes", "categoria_principal", "nutrition"
         ])
         for recipe in recipes:
             writer.writerow([
                 recipe["url"], recipe["pais"], recipe["titulo"], recipe["fecha"], recipe["autor"],
-                recipe["descripcion_corta"], recipe["calificacion_media"], recipe["num_reviews"],
-                recipe["preparacion"], recipe["ingredientes"], recipe["categoria_principal"],
-                recipe["nutrition"]
+                recipe["calificacion_media"], recipe["num_reviews"],recipe["realizado_casa"],recipe["preparacion"], 
+                recipe["ingredientes"], recipe["categoria_principal"], recipe["nutrition"]
             ])
+
 
