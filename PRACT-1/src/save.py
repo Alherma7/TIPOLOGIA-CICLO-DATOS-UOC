e07d0a6e3b3c7745c676ad99e8391d7cf7583124
Alherma7 <ahernandezmai@uoc.edu>
2025-10-31 15:32:45 +0100

    Update save.py

--- PRACT-1/src/save.py
+++ PRACT-1/src/save.py
@@ -1,7 +1,18 @@
 #save.py
+import csv
 
 def save_recipes(recipes, csv_path="Recetas.csv"):
+    """
+    Guarda una lista de recetas en un archivo CSV con formato delimitado por punto y coma.
 
+    Args:
+        recipes (list): Lista de diccionarios, donde cada diccionario representa una receta con campos.
+        csv_path (str): Ruta y nombre del archivo CSV de salida. Por defecto es "Recetas01.csv".
+
+    Returns:
+        None
+    """
+    
     with open(csv_path, "w", encoding="utf-8", newline="") as f_csv:
         writer = csv.writer(f_csv, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow([
@@ -11,8 +22,9 @@ def save_recipes(recipes, csv_path="Recetas.csv"):
         for recipe in recipes:
             writer.writerow([
                 recipe["url"], recipe["pais"], recipe["titulo"], recipe["fecha"], recipe["autor"],
-                recipe["calificacion_media"], recipe["num_reviews"],recipe["realizado_casa"],recipe["preparacion"], 
+                recipe["calificacion_media"], recipe["num_reviews"],recipe["realizado_casa"],recipe["preparacion"],
                 recipe["ingredientes"], recipe["categoria_principal"], recipe["nutrition"]
             ])
+
 
 
